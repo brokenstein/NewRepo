f4b261c30abdc96820088ba40d6589e6e262cdb7
krux <gaztambideraul@gmail.com>
2022-01-02 09:27:04 -0500

    Testing functionality of github & VS

---
+++ index.js
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <b>This is the test</b>
+    </head>
+    <body>
+        Here we type some shit
+        Here we Copyright some shit <script>document.write( new Date().getUTCFullYear() );</script>
+    </body>
+    <footer>
+        Put foot shit
+    </footer>
+</html>
